d9efe0da91d1c8038b8e2b37484ece8fb489449c
Alina Sapronova <106851105+AlinaSapronova@users.noreply.github.com>
2023-09-29 18:52:18 +0000

    create,update,delete for directors done

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
 
   get("/directors/youngest", { :controller => "directors", :action => "max_dob" })
   get("/directors/eldest", { :controller => "directors", :action => "min_dob" })
-
+  post("/insert_director", { :controller => "directors", :action => "create" })
   get("/directors", { :controller => "directors", :action => "index" })
   get("/directors/:path_id", { :controller => "directors", :action => "show" })
+  post("/modify_director/:path_id", { :controller => "directors", :action => "update" })
+  get("/delete_director/:path_id", { :controller => "directors", :action => "destroy" })
 
   get("/movies", { :controller => "movies", :action => "index" })
   get("/movies/:path_id", { :controller => "movies", :action => "show" })
