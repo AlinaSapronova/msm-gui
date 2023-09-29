a864240371e5d4a9aa5cc07d93bfb9b742d92988
Alina Sapronova <106851105+AlinaSapronova@users.noreply.github.com>
2023-09-29 19:29:28 +0000

    create,update,delete for actors are done!

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -14,4 +14,40 @@ class ActorsController < ApplicationController
       
     render({ :template => "actor_templates/show" })
   end
+
+  def create
+    @new_actor = Actor.new
+    @new_actor = params.fetch("query_name")
+    @new_actor = params.fetch("query_dob")
+    @new_actor = params.fetch("query_bio")
+    @new_actor = params.fetch("query_image")
+
+    redirect_to("/actors")
+  end
+
+  def update
+    actor_id = params.fetch("path_id")
+    @the_actor = Actor.where({:id => actor_id})[0]
+
+    @the_actor.name = params.fetch("query_name")
+    @the_actor.dob = params.fetch("query_dob")
+    @the_actor.bio = params.fetch("query_bio")
+    @the_actor.image = params.fetch("query_image")
+    @the_actor.save
+
+    redirect_to("/actors")
+
+  end
+
+  def destroy
+    actor_id = params.fetch("path_id")
+    @the_actor = Actor.where({:id => actor_id})[0]
+
+    @the_actor.destroy
+
+    redirect_to("/actors")
+  end
+
+
+
 end
